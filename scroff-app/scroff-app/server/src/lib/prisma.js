eed0a819c4d6c2c9a70047be3c4a4a22d90347ac
RigelMyMarketing <rigel.mymkting@gmail.com>
2026-07-29 00:49:50 +0800

    Delete scroff-app directory

--- scroff-app/scroff-app/server/src/lib/prisma.js
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-// A single shared Prisma instance for the whole process. Avoids exhausting
-// database connections when files get hot-reloaded in dev.
-export const prisma = new PrismaClient();
